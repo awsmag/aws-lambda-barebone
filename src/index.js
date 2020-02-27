5ebc06aef6c7222d75cb5b56b5ff069d375c25ed
Simranjeet Singh <simranjeet2589@gmail.com>
2020-02-27 11:22:13 +0530

    [setup gulp and mocha]

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 async function handleEvent(event) {
   // write code here
-  console.log("Hello Lambda!!!")
+  console.log("Hello Lambda!!!", event); // eslint-disable-line no-console
 }
 
 module.exports = {handleEvent};
